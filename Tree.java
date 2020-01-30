5049b90a13ccf6958ef4f661c22ff7f75a97fff6
jthorare <60298547+jthorare@users.noreply.github.com>
2020-01-29 22:32:15 -0500

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,27 +1,29 @@
 import tester.*;
-import javalib.worldimages.*;   // images, like RectangleImage or OverlayImages
-import javalib.funworld.*;      // the abstract World class and the big-bang library
-import java.awt.Color; 
- 
-interface ITree { /* see methods below */ 
-  WorldImage draw(); // OverlayOffsetAlign = pinhole to pinhole
+import javalib.worldimages.*; // images, like RectangleImage or OverlayImages
+import javalib.funworld.*; // the abstract World class and the big-bang library
+import javalib.worldcanvas.WorldCanvas;
+
+import java.awt.Color;
+
+interface ITree { /* see methods below */
+  WorldImage draw();
 }
- 
+
 class Leaf implements ITree {
   int size; // represents the radius of the leaf
   Color color; // the color to draw it
-  
+
   Leaf(int size, Color color) {
     this.size = size;
     this.color = color;
   }
-  
+
   // leaves are RED Color.RED
   public WorldImage draw() {
     return new CircleImage(this.size, OutlineMode.SOLID, this.color);
   }
 }
- 
+
 class Stem implements ITree {
   // How long this stick is
   int length;
@@ -29,18 +31,22 @@ class Stem implements ITree {
   double theta;
   // The rest of the tree
   ITree tree;
-  
+
   Stem(int length, double theta, ITree tree) {
     this.length = length;
     this.theta = theta;
     this.tree = tree;
   }
-  
+
   public WorldImage draw() {
-    return new LineImage(new Posn((int)Math.round((this.length * Math.cos(this.theta))), (int)Math.round((this.length * Math.sin(this.theta)))), Color.BLACK);
+    return new AboveAlignImage(AlignModeX.CENTER, new VisiblePinholeImage(this.tree.draw()),
+        new VisiblePinholeImage(new LineImage(
+            new Posn((int) Math.round((this.length * Math.cos(Math.toRadians(this.theta)))),
+                (int) Math.round((this.length * Math.sin(Math.toRadians(this.theta))))),
+            Color.BLACK).movePinhole(0, -(this.length / 2))));
   }
 }
- 
+
 class Branch implements ITree {
   // How long the left and right branches are
   int leftLength;
@@ -51,30 +57,56 @@ class Branch implements ITree {
   // The remaining parts of the tree
   ITree left;
   ITree right;
-  
-  Branch(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree left, ITree right) {
+
+  Branch(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree left,
+      ITree right) {
     this.leftLength = leftLength;
-     this.rightLength = rightLength;
-     this.leftTheta = leftTheta;
-     this.rightTheta = rightTheta;
-     this.left = left;
-     this.right = right;
+    this.rightLength = rightLength;
+    this.leftTheta = leftTheta;
+    this.rightTheta = rightTheta;
+    this.left = left;
+    this.right = right;
   }
-  
+
   public WorldImage draw() {
-    return new LineImage
+    return new OverlayImage(AlignModeY.BOTTOM,(new RotateImage(this.drawBranchLeft(), 90 - leftTheta)),
+        new RotateImage(this.drawBranchRight(), 90 - rightTheta));
+  }
+
+  WorldImage drawBranchLeft() {
+    return new OverlayImage(this.left.draw(),
+        new LineImage(new Posn(0, leftLength), Color.BLACK).movePinhole(0, -this.leftLength / 2));
+  }
+
+  WorldImage drawBranchRight() {
+    return new OverlayImage(this.right.draw(),
+        new LineImage(new Posn(0, rightLength), Color.BLACK).movePinhole(0, -this.rightLength / 2));
   }
 }
 
 class ExamplesTree {
-  ExamplesTree() {}
+  ExamplesTree() {
+  }
+
   ITree tree1 = new Branch(30, 30, 135, 40, new Leaf(10, Color.RED), new Leaf(15, Color.BLUE));
   ITree tree2 = new Branch(30, 30, 115, 65, new Leaf(15, Color.GREEN), new Leaf(8, Color.ORANGE));
-
+  ITree tree3 = new Stem(40, 75, new Leaf(15, Color.BLUE));
+  ITree tree4 = new Branch(30, 30, 225,45, new Leaf(15, Color.GREEN), new Leaf(8, Color.ORANGE));
   ITree tree1Stem = new Stem(40, 90, tree1);
   ITree tree2Stem = new Stem(50, 90, tree2);
 
   boolean testDraw(Tester t) {
-    return t.checkExpect(this.tree1.draw(this.tree2), new Branch(40, 50, 150, 30, tree1, tree2));
+    return t.checkExpect(this.tree1.draw(), new Branch(40, 50, 150, 30, tree1, tree2));
+  }
+
+  /*
+   * boolean testDrawTree(Tester t) { WorldCanvas c = new WorldCanvas(500, 500);
+   * WorldScene s = new WorldScene(500, 500); return
+   * c.drawScene(s.placeImageXY(tree2Stem.draw(), 250, 250)) && c.show(); }
+   */
+  boolean testDrawTree(Tester t) {
+    WorldCanvas c = new WorldCanvas(500, 500);
+    WorldScene s = new WorldScene(500, 500);
+    return c.drawScene(s.placeImageXY(tree4.draw(), 250, 250)) && c.show();
   }
 }
